6ad13cc0b4a7df87dad3a77ce73c05166759e13b
Demetrius Nekos <dnekos2462@gmail.com>
2023-02-08 20:52:05 -0500

    struggling to get controller working in update

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode0_Starter/a3_DemoMode0_Starter-load.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode0_Starter/a3_DemoMode0_Starter-load.c
@@ -179,6 +179,7 @@ void a3starter_load(a3_DemoState const* demoState, a3_DemoMode0_Starter* demoMod
 	for (int i = 0; i < 64; i++)
 	{
 		demoMode->keyframePool->keyframe[i].data = i;
+		demoMode->keyframePool->keyframe[i].index = i;
 	}
 
 	// create clip
@@ -198,6 +199,11 @@ void a3starter_load(a3_DemoState const* demoState, a3_DemoMode0_Starter* demoMod
 	a3clipDistributeDuration(&demoMode->clipPool->clip[1], 2);
 	a3clipTransitionInit(&demoMode->clipPool->clip[1].forward_transition, demoMode->clipPool, 0);
 
+
+	// set clip on controller
+	a3clipControllerSetClip(demoMode->clipController, demoMode->clipPool, demoMode->clipPool->clip[0].index);
+	// play forward
+	demoMode->clipController->playback_direction = 1;
 }
 
 
